32f6c5c4866ae6ae875e3c0225f757376712fb0c
Mallycon <45456087+Mallycon@users.noreply.github.com>
2021-03-18 15:28:58 +0100

    ...

---
+++ Noscent/Assets/WhackAScent/Scripts/WASSpawn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WASSpawn : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    public void GoTime()
+    {
+        // if (this.transform.position.y < )
+    }
+}
